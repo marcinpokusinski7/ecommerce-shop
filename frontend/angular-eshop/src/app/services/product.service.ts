a69f4e4137003d0ad23ee393fa3564e869162a5a
marcinpokusinski7 <marcinpokusinski@gmail.com>
2021-04-01 00:48:25 +0200

    updated findByCategoryId query

--- frontend/angular-eshop/src/app/services/product.service.ts
+++ frontend/angular-eshop/src/app/services/product.service.ts
@@ -13,8 +13,10 @@ export class ProductService {
 
   constructor(private httpClient: HttpClient) { }
 
-  getProductList(): Observable<Product[]>{
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+  getProductList(theCategoryId: number): Observable<Product[]>{
+    // build url based on category id
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+    return this.httpClient.get<GetResponse>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
   }
@@ -25,3 +27,4 @@ interface GetResponse {
     products: Product[];
   };
 }
+
